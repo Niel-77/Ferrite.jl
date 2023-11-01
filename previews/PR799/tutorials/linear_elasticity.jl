f24ef91ed7152c3ea6932ab8d5072a4a8d04a9cc
Documenter.jl <documenter@juliadocs.github.io>
2023-11-01 11:18:59 +0000

    build based on 52da05f

--- previews/PR799/tutorials/linear_elasticity.jl
+++ previews/PR799/tutorials/linear_elasticity.jl
@@ -22,36 +22,23 @@ add!(ch, Dirichlet(:u, getfaceset(grid, "left"), (x, t) -> 0.0, 1))
 add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
 close!(ch);
 
-struct Elasticity
-    G::Float64
-    K::Float64
-end
-
-function material_routine(material::Elasticity, ε::SymmetricTensor{2})
-    (; G, K) = material
-    stress(ε) = 2G * dev(ε) + K * tr(ε) * one(ε)
-    ∂σ∂ε, σ = gradient(stress, ε, :all)
-    return σ, ∂σ∂ε
-end
-
 E = 200e3 # Young's modulus [MPa]
 ν = 0.3 # Poisson's ratio [-]
-material = Elasticity(E/2(1+ν), E/3(1-2ν));
 
-function assemble_cell!(ke, fe, cellvalues, material, ue)
+λ = E*ν / ((1 + ν) * (1 - 2ν)) # 1st Lamé parameter
+μ = E / (2(1 + ν)) # 2nd Lamé parameter
+I = one(SymmetricTensor{2, dim}) # 2nd order unit tensor
+II = one(SymmetricTensor{4, dim}) # 4th order symmetric unit tensor
+∂σ∂ε = 2μ * II + λ * (I ⊗ I) # elastic stiffness tensor
+
+function assemble_cell!(ke, cellvalues, ∂σ∂ε)
     fill!(ke, 0.0)
-    fill!(fe, 0.0)
 
     n_basefuncs = getnbasefunctions(cellvalues)
     for q_point in 1:getnquadpoints(cellvalues)
-        # For each integration point, compute strain, stress and material stiffness
-        ε = function_symmetric_gradient(cellvalues, q_point, ue)
-        σ, ∂σ∂ε = material_routine(material, ε)
-
         dΩ = getdetJdV(cellvalues, q_point)
         for i in 1:n_basefuncs
             ∇Nᵢ = shape_gradient(cellvalues, q_point, i)# shape_symmetric_gradient(cellvalues, q_point, i)
-            fe[i] += σ ⊡ ∇Nᵢ * dΩ # add internal force to residual
             for j in 1:n_basefuncs
                 ∇ˢʸᵐNⱼ = shape_symmetric_gradient(cellvalues, q_point, j)
                 ke[i, j] += (∂σ∂ε ⊡ ∇ˢʸᵐNⱼ) ⊡ ∇Nᵢ * dΩ
@@ -60,36 +47,34 @@ function assemble_cell!(ke, fe, cellvalues, material, ue)
     end
 end
 
-function assemble_global!(K, f, a, dh, cellvalues, material)
-    # Allocate the element stiffness matrix and element force vector
+function assemble_global!(K, dh, cellvalues, ∂σ∂ε)
+    # Allocate the element stiffness matrix
     n_basefuncs = getnbasefunctions(cellvalues)
     ke = zeros(n_basefuncs, n_basefuncs)
-    fe = zeros(n_basefuncs)
     # Create an assembler
-    assembler = start_assemble(K, f)
+    assembler = start_assemble(K)
     # Loop over all cells
     for cell in CellIterator(dh)
         reinit!(cellvalues, cell) # update spatial derivatives based on element coordinates
-        @views ue = a[celldofs(cell)]
         # Compute element contribution
-        assemble_cell!(ke, fe, cellvalues, material, ue)
+        assemble_cell!(ke, cellvalues, ∂σ∂ε)
         # Assemble ke and fe into K and f
-        assemble!(assembler, celldofs(cell), ke, fe)
+        assemble!(assembler, celldofs(cell), ke)
     end
-    return K, f
+    return K
 end
 
 K = create_sparsity_pattern(dh)
-f = zeros(ndofs(dh))
-a = zeros(ndofs(dh))
-assemble_global!(K, f, a, dh, cellvalues, material);
+assemble_global!(K, dh, cellvalues, ∂σ∂ε);
+
+f = zeros(ndofs(dh));
 
 apply!(K, f, ch)
 u = K \ f;
 
 vtk_grid("linear_elasticity", dh) do vtk
     vtk_point_data(vtk, dh, u)
-    vtk_cellset(vtk, grid)
+    vtk_cellset(vtk, grid) # export cellsets of grains for logo-coloring
 end
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl

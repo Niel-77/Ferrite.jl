1fa8e380c249f018541d3cd6c219e21018cef640
Documenter.jl <documenter@juliadocs.github.io>
2023-11-02 14:10:59 +0000

    build based on 9d1d780

--- previews/PR799/literate-tutorials/linear_elasticity.jl
+++ previews/PR799/literate-tutorials/linear_elasticity.jl
@@ -73,8 +73,9 @@ FerriteGmsh.Gmsh.gmsh.option.set_number("General.Verbosity", 2) #hide
 grid = togrid("logo.geo");
 #md nothing # hide
 # By default the grid lacks the facesets for the boundaries, so we add them by Ferrite here.
+# [`addfaceset!`](@ref) allows to add facesets to the grid based on coordinates.
 # Note that approximate comparison to 0.0 doesn't work well, so we use a tolerance instead.
-addfaceset!(grid, "top", x->x[2] ≈ 1.0)
+addfaceset!(grid, "top", x->x[2] ≈ 1.0) # faces for which x[2] ≈ 1.0 for all nodes
 addfaceset!(grid, "left", x->x[1] < 1e-6) 
 addfaceset!(grid, "bottom", x->x[2] < 1e-6);
 
@@ -100,7 +101,9 @@ dim = 2
 order = 1 # linear interpolation
 ip = Lagrange{RefTriangle, order}()^dim # vector valued interpolation
 qr = QuadratureRule{RefTriangle}(1) # 1 quadrature point
-cellvalues = CellValues(qr, ip);
+cellvalues = CellValues(qr, ip)
+qr_face = FaceQuadratureRule{RefTriangle}(1)
+facevalues = FaceValues(qr_face, ip);
 
 # ### Degrees of freedom
 # For distributing degrees of freedom, we define a `DofHandler`. The `DofHandler` knows that
@@ -117,7 +120,7 @@ close!(dh);
 ch = ConstraintHandler(dh)
 add!(ch, Dirichlet(:u, getfaceset(grid, "bottom"), (x, t) -> 0.0, 2))
 add!(ch, Dirichlet(:u, getfaceset(grid, "left"), (x, t) -> 0.0, 1))
-add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
+#add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
 close!(ch);
 
 # ### Material behavior
@@ -160,7 +163,6 @@ II = one(SymmetricTensor{4, dim}) # 4th order symmetric unit tensor
 # and can then be used for all integration points.
 function assemble_cell!(ke, cellvalues, ∂σ∂ε)
     fill!(ke, 0.0)
-
     n_basefuncs = getnbasefunctions(cellvalues)
     for q_point in 1:getnquadpoints(cellvalues)
         dΩ = getdetJdV(cellvalues, q_point)
@@ -172,6 +174,7 @@ function assemble_cell!(ke, cellvalues, ∂σ∂ε)
             end
         end
     end
+    return ke
 end
 #md nothing # hide
 
@@ -198,20 +201,40 @@ function assemble_global!(K, dh, cellvalues, ∂σ∂ε)
 end
 #md nothing # hide
 
+function assemble_external_forces!(f_ext, dh, faceset, facevalues, prescribed_traction)
+    fe_ext = zeros(getnbasefunctions(facevalues))
+    for face in FaceIterator(dh, faceset)
+        reinit!(facevalues, face)
+        fill!(fe_ext, 0.0)
+        for qp in 1:getnquadpoints(facevalues)
+            X = spatial_coordinate(facevalues, qp, getcoordinates(face))
+            tₚ = prescribed_traction(X)
+            dΓ = getdetJdV(facevalues, qp)
+            for i in 1:getnbasefunctions(facevalues)
+                Nᵢ = shape_value(facevalues, qp, i)
+                fe_ext[i] += tₚ ⋅ Nᵢ * dΓ
+            end
+        end
+        assemble!(f_ext, celldofs(face), fe_ext)
+    end
+    return f_ext
+end
+#md nothing # hide
+
 # ### Solution of the system
 # The last step is to solve the system. First we allocate the global stiffness matrix `K`
 # and assemble it.
 K = create_sparsity_pattern(dh)
 assemble_global!(K, dh, cellvalues, ∂σ∂ε);
-# Then we allocate the external force vector. Since we don't apply any external forces,
-# it is a zero vector in this case.
-f = zeros(ndofs(dh));
+# Then we allocate the external force vector. 
+f_ext = zeros(ndofs(dh));
+assemble_external_forces!(f_ext, dh, getfaceset(grid, "top"), facevalues, x->Vec(0.0, 20e3*x[1]))
 
 # To account for the Dirichlet boundary conditions we use the `apply!` function.
 # This modifies elements in `K` and `f` respectively, such that
 # we can get the correct solution vector `u` by using `\`.
-apply!(K, f, ch)
-u = K \ f;
+apply!(K, f_ext, ch)
+u = K \ f_ext;
 
 # ### Exporting to VTK
 # To visualize the result we export the grid and our field `u`
--- previews/PR799/tutorials/linear_elasticity.jl
+++ previews/PR799/tutorials/linear_elasticity.jl
@@ -4,7 +4,7 @@ FerriteGmsh.Gmsh.initialize() # hide
 FerriteGmsh.Gmsh.gmsh.option.set_number("General.Verbosity", 2) #hide
 grid = togrid("logo.geo");
 
-addfaceset!(grid, "top", x->x[2] ≈ 1.0)
+addfaceset!(grid, "top", x->x[2] ≈ 1.0) # faces for which x[2] ≈ 1.0 for all nodes
 addfaceset!(grid, "left", x->x[1] < 1e-6)
 addfaceset!(grid, "bottom", x->x[2] < 1e-6);
 
@@ -12,7 +12,9 @@ dim = 2
 order = 1 # linear interpolation
 ip = Lagrange{RefTriangle, order}()^dim # vector valued interpolation
 qr = QuadratureRule{RefTriangle}(1) # 1 quadrature point
-cellvalues = CellValues(qr, ip);
+cellvalues = CellValues(qr, ip)
+qr_face = FaceQuadratureRule{RefTriangle}(1)
+facevalues = FaceValues(qr_face, ip);
 
 dh = DofHandler(grid)
 add!(dh, :u, ip)
@@ -21,7 +23,7 @@ close!(dh);
 ch = ConstraintHandler(dh)
 add!(ch, Dirichlet(:u, getfaceset(grid, "bottom"), (x, t) -> 0.0, 2))
 add!(ch, Dirichlet(:u, getfaceset(grid, "left"), (x, t) -> 0.0, 1))
-add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
+#add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
 close!(ch);
 
 E = 200e3 # Young's modulus [MPa]
@@ -35,7 +37,6 @@ II = one(SymmetricTensor{4, dim}) # 4th order symmetric unit tensor
 
 function assemble_cell!(ke, cellvalues, ∂σ∂ε)
     fill!(ke, 0.0)
-
     n_basefuncs = getnbasefunctions(cellvalues)
     for q_point in 1:getnquadpoints(cellvalues)
         dΩ = getdetJdV(cellvalues, q_point)
@@ -47,6 +48,7 @@ function assemble_cell!(ke, cellvalues, ∂σ∂ε)
             end
         end
     end
+    return ke
 end
 
 function assemble_global!(K, dh, cellvalues, ∂σ∂ε)
@@ -66,13 +68,33 @@ function assemble_global!(K, dh, cellvalues, ∂σ∂ε)
     return K
 end
 
+function assemble_external_forces!(f_ext, dh, faceset, facevalues, prescribed_traction)
+    fe_ext = zeros(getnbasefunctions(facevalues))
+    for face in FaceIterator(dh, faceset)
+        reinit!(facevalues, face)
+        fill!(fe_ext, 0.0)
+        for qp in 1:getnquadpoints(facevalues)
+            X = spatial_coordinate(facevalues, qp, getcoordinates(face))
+            tₚ = prescribed_traction(X)
+            dΓ = getdetJdV(facevalues, qp)
+            for i in 1:getnbasefunctions(facevalues)
+                Nᵢ = shape_value(facevalues, qp, i)
+                fe_ext[i] += tₚ ⋅ Nᵢ * dΓ
+            end
+        end
+        assemble!(f_ext, celldofs(face), fe_ext)
+    end
+    return f_ext
+end
+
 K = create_sparsity_pattern(dh)
 assemble_global!(K, dh, cellvalues, ∂σ∂ε);
 
-f = zeros(ndofs(dh));
+f_ext = zeros(ndofs(dh));
+assemble_external_forces!(f_ext, dh, getfaceset(grid, "top"), facevalues, x->Vec(0.0, 20e3*x[1]))
 
-apply!(K, f, ch)
-u = K \ f;
+apply!(K, f_ext, ch)
+u = K \ f_ext;
 
 vtk_grid("linear_elasticity", dh) do vtk
     vtk_point_data(vtk, dh, u)
